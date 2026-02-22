39965b220ed128d4cb47f9e36909c2168b676d8e
Mike <mike@example.com>
2026-02-21 22:41:24 -0500

    Increase z-index of audio player

--- components/audio-player.tsx
+++ components/audio-player.tsx
@@ -247,7 +247,7 @@ export function AudioPlayer({ track, onClose, onNext, onPrevious, hasNext, hasPr
 
   return (
     <>
-      <div className="fixed bottom-0 left-0 right-0 z-50">
+      <div className="fixed bottom-0 left-0 right-0 z-[60]">
         {/* Progress Bar at Top */}
         <div className="h-1 bg-white/10 cursor-pointer group">
           <div 
